a71c7c3fe8af1786ef88f1c6b33f770a0d9f8ab0
jessie vegas <j.ekaf@hotmail.com>
2016-02-18 12:44:56 +0000

    No form1

--- jogo_FV/jogo_FV/Form1.Designer.cs
+++ jogo_FV/jogo_FV/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 60);
+            this.button2.Location = new System.Drawing.Point(54, 89);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
